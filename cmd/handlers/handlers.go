8a5e36424597b543b11a816a2acaee37943c3355
Danila <84472737+dupreehkuda@users.noreply.github.com>
2022-07-13 10:31:44 +0300

    increment_3 initial

--- cmd/handlers/handlers.go
+++ cmd/handlers/handlers.go
@@ -2,53 +2,44 @@ package handlers
 
 import (
 	"fmt"
+	"github.com/gin-gonic/gin"
 	"io"
 	"net/http"
 	"strconv"
-	"strings"
 )
 
-func trimmer(path string) int {
-	id := strings.TrimPrefix(path, "/")
-	i, err := strconv.Atoi(id)
+func GetShortened(storage map[int]string) gin.HandlerFunc {
+	fn := func(c *gin.Context) {
+		id, err := strconv.Atoi(c.Param("id"))
+		if err != nil {
+			return
+		}
 
-	if err != nil {
-		return 0
+		if len(storage) == 0 || id == 0 || storage[id] == "" {
+			c.Data(http.StatusBadRequest, "text/plain; charset=utf-8", []byte("Can't find url requested"))
+			return
+		}
+
+		c.Header("Location", storage[id])
+		c.JSON(http.StatusTemporaryRedirect, nil)
 	}
 
-	return i
+	return gin.HandlerFunc(fn)
 }
 
-func APIResponse(storage map[int]string) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		switch r.Method {
-		case http.MethodGet:
-			i := trimmer(r.URL.Path)
+func PostShorten(storage map[int]string) gin.HandlerFunc {
+	fn := func(c *gin.Context) {
+		b, err := io.ReadAll(c.Request.Body)
 
-			if len(storage) == 0 || i == 0 || storage[i] == "" {
-				w.WriteHeader(http.StatusBadRequest)
-				w.Write([]byte("Can't find url requested"))
-				return
-			}
-
-			w.Header().Set("Location", storage[i])
-			w.WriteHeader(http.StatusTemporaryRedirect)
-
-		case http.MethodPost:
-			b, err := io.ReadAll(r.Body)
-			if err != nil || len(b) == 0 {
-				w.WriteHeader(http.StatusBadRequest)
-				return
-			}
-
-			storage[len(storage)+1] = string(b)
-			string := fmt.Sprintf("http://localhost:8080/%v", len(storage))
-			w.WriteHeader(http.StatusCreated)
-			w.Write([]byte(string))
-
-		default:
-			w.WriteHeader(http.StatusBadRequest)
-			w.Write([]byte("Can't find method requested"))
+		if err != nil || len(b) == 0 {
+			c.JSON(http.StatusBadRequest, nil)
+			return
 		}
+
+		storage[len(storage)+1] = string(b)
+		string := fmt.Sprintf("http://localhost:8080/%v", len(storage))
+		c.Data(http.StatusCreated, "text/plain; charset=utf-8", []byte(string))
 	}
+
+	return gin.HandlerFunc(fn)
 }
