e0096eed9333a3e82d710752a96ec0ab8cd50119
Danila <84472737+dupreehkuda@users.noreply.github.com>
2022-07-27 15:59:06 +0300

    increment_5 initial

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,15 +1,40 @@
 package main
 
 import (
+	"github.com/caarlos0/env/v6"
 	"github.com/dupreehkuda/url-shortener/internal/handlers"
 	"github.com/dupreehkuda/url-shortener/internal/server"
-	"github.com/dupreehkuda/url-shortener/internal/storage"
+	"github.com/dupreehkuda/url-shortener/internal/storage/filestore"
+	"github.com/dupreehkuda/url-shortener/internal/storage/memstore"
+	"log"
 )
 
-func main() {
-	storage := storage.New()
-	service := handlers.New(storage)
+var cfg struct {
+	Addr            string `env:"SERVER_ADDRESS" envDefault:"localhost:8080"`
+	BaseURL         string `env:"BASE_URL" envDefault:"http://localhost:8080"`
+	FileStoragePath string `env:"FILE_STORAGE_PATH"`
+}
 
+func main() {
+
+	var err = env.Parse(&cfg)
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	var storage handlers.Storer
+
+	if cfg.FileStoragePath != "" {
+		storage = filestore.New(cfg.FileStoragePath)
+		log.Printf("Launched with file")
+	} else {
+		storage = memstore.New()
+		log.Printf("Launched with memory")
+	}
+
+	service := handlers.New(storage)
 	server := server.New(service)
+
 	server.Launch()
 }
--- internal/storage/memstore/memstore_test.go
+++ internal/storage/memstore/memstore_test.go
@@ -1,4 +1,4 @@
-package storage
+package memstore
 
 import (
 	"sync"
